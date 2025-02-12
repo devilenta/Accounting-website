031701910317bdb38cd9166e10f49cfaca0360cc
evolenta <mohsenhasani116@gmail.com>
2025-02-12 13:21:35 +0330

    insert first feature

--- script/script_home.js
+++ script/script_home.js
@@ -1,5 +1,6 @@
 let btn_main = document.querySelector(".btn_main")
-
+let box = document.querySelectorAll(".box")
+let selector = document.querySelectorAll(".selector")
  
 
 
@@ -10,9 +11,95 @@ btn_main.addEventListener("click",() =>{
 })
 
 
+box.forEach((box_click)=>{
+    
+    window.addEventListener("load",function(){
 
-// btn_main.addEventListener("mousemove" ,()=>{
+        box_click.style.transform = "translateX(31vw)"
+        
+    })
+    
 
-// console.log("salam");
+    box_click.addEventListener("mousemove",function(){
 
-// })
+
+        box_click.classList.add("box_selected")
+        
+    })
+
+
+    box_click.addEventListener("starttouch",function(){
+
+        
+            box_click.classList.remove("box_selected")
+
+        box_click.classList.add("box_selected")
+        
+    })
+   
+
+    box_click.addEventListener("mouseleave",function(){
+
+
+        box_click.classList.remove("box_selected")
+        
+    })
+
+
+
+    box_click.addEventListener("click",function(){
+        
+        remove_selected_func(selector ,box_click)
+        move_box_func(box, box_click )
+       
+    })
+
+   
+
+
+
+})
+
+
+
+
+ 
+function remove_selected_func(circle , box_click){
+
+    circle.forEach(function(gh){
+
+
+        gh.classList.remove("selector_main")
+    })
+    circle[box_click.dataset.a].classList.add("selector_main")
+}
+///////////////////////////////////////////////////////
+function move_box_func(box , box_click){
+
+    box.forEach(function(ghum){
+        if(box_click.dataset.a == 1){
+
+
+            ghum.style.transform = "translateX(0vw)"
+
+
+         }
+
+         if(box_click.dataset.a == 2){
+
+
+             ghum.style.transform = "translateX(31vw)"
+
+
+          }
+
+
+         if(box_click.dataset.a == 3){
+
+
+            ghum.style.transform = "translateX(62vw)"
+    
+          }
+
+        })
+}
